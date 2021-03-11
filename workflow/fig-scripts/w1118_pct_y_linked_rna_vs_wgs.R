b1a9a615eeb38f29cb99d10625667fe1261c1b70
mal2017 <matt.a.lawlor@gmail.com>
2021-03-11 14:39:16 -0500

    cleaner y-linked pct approach

--- workflow/fig-scripts/w1118_pct_y_linked_rna_vs_wgs.R
+++ workflow/fig-scripts/w1118_pct_y_linked_rna_vs_wgs.R
@@ -25,45 +25,47 @@ tep_tes <- geps %>%
   pull(gene_id) %>%
   unique()
 
-scrna.detected.tes <- geps %>% filter(!str_detect(X1,'FBgn')) %>% dplyr::select(X1) %>% left_join(te.lookup, by=c(X1='merged_te')) %>% pull(gene_id)
-
 rna <- Sys.glob('results/finalized/w1118-testes-total-rna/rep*-depth-at-male-snps/') %>%
   set_names(.,str_extract(.,"(?<=rna\\/)rep\\d+")) %>%
   map_df(~collect(open_dataset(., format='arrow'))) %>%
-  distinct() %>% # get rid of apparent dup rows caused by multiple male-specific snps in same pos
   spread(.,sex, depth, fill = 0) %>%
-  mutate(.,pct.male=male/(unknown + male))
+  mutate(.,pct.male=male/(unknown + male), ratio.male=male/unknown)
 
 dna <- open_dataset('results/finalized/wgs/w1118_male/snp_depths/', format='arrow') %>% 
   collect() %>%
   spread(.,sex, depth, fill = 0) %>%
-  mutate(.,pct.male=male/(unknown + male))
+  mutate(.,pct.male=male/(unknown + male), ratio.male=male/unknown)
 
-res2 <-inner_join(dna, rna, by=c('seqnames','pos'), suffix=c('.dna','.tx')) %>%
-  group_by(sample.tx, seqnames) %>% 
-  summarise(pct.male.dna=mean(pct.male.dna), pct.male.tx=mean(pct.male.tx), .groups = 'drop')
+df <- left_join(dna, rna, by=c('seqnames','pos'), suffix=c('.dna','.tx')) %>%
+  group_by(subsample, sample.tx, seqnames) %>% 
+  summarise(pct.male.dna=mean(pct.male.dna), pct.male.tx=mean(pct.male.tx),
+            ratio.male.dna=mean(ratio.male.dna), ratio.male.tx=mean(ratio.male.tx), .groups = 'drop')
 
-res3 <- res2%>% mutate(gep=ifelse(seqnames %in% tep_tes,'tep','other'))
+df <- te.lookup %>% dplyr::select(merged_te, gene_id) %>% distinct() %>%
+  left_join(df, by=c(gene_id = 'seqnames')) %>%
+  complete(gene_id, nesting(sample.tx, subsample)) %>%
+  filter(!is.na(sample.tx)) %>%
+  mutate(gep=ifelse(gene_id %in% tep_tes,'tep','other')) %>%
+  mutate_at(vars(c('pct.male.dna','pct.male.tx','ratio.male.dna','ratio.male.tx')), replace_na, 0) %>%
+  filter(!str_detect(gene_id,'[-_]LTR'))
 
-df <- res3 %>%
-  filter(!str_detect(seqnames,'[-_]LTR')) %>%
-  filter(seqnames %in% scrna.detected.tes)
 
-g0 <- df %>% mutate(ratio=pct.male.tx/pct.male.dna) %>% 
-  ggplot(aes(gep, ratio)) +
+g0 <- df %>%
+  ggplot(aes(gep, pct.male.tx/pct.male.dna)) +
   geom_boxplot(aes(fill=gep)) +
   stat_compare_means() +
   theme_classic() +
-  theme(aspect.ratio = 1) + ylab("% male depth at male SNPs (RNA/WGS)") + xlab('') +
-  scale_fill_brewer(type='qual', name='GEP')
+  theme(aspect.ratio = 1) + ylab("% Male Depth (Male Allele/DNA)") + xlab('') +
+  scale_fill_brewer(type='qual', name='GEP') +
+  facet_wrap(~subsample)
 
 
-g <- ggplot(df, aes(pct.male.dna,pct.male.tx, label=seqnames)) +
+g <- ggplot(df, aes(pct.male.dna,pct.male.tx, label=gene_id)) +
   geom_point(aes(color=gep)) +
   facet_wrap(~gep, scales = 'free') +
   stat_cor(method = 'spearman') + 
   theme_classic() +
-  ggrepel::geom_text_repel(force = 2) +
+  #ggrepel::geom_text_repel(force = 2) +
   geom_abline(slope=1, intercept=0, color='lightgray', linetype='dashed') +
   theme(aspect.ratio = 1) +
   scale_color_brewer(type='qual', name='GEP') +
@@ -71,7 +73,8 @@ g <- ggplot(df, aes(pct.male.dna,pct.male.tx, label=seqnames)) +
   scale_y_continuous(labels=scales::percent) +
   xlab('WGS mean male SNP depth') +
   ylab('RNA-seq mean male SNP depth')  +
-  guides(color=F)
+  guides(color=F) +
+  facet_grid(gep~subsample)
 
 agg_png(snakemake@output[['png']], width=10, height =10, units = 'in', scaling = 1.5, bitsize = 16, res = 300, background = 'transparent')
 print(g)
