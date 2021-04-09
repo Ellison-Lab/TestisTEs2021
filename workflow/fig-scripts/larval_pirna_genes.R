8e1af7cee02d0457b8aa6b5bf2681483efe426fc
mal2017 <matt.a.lawlor@gmail.com>
2021-04-09 14:08:54 -0400

    update pirna dotplot

--- workflow/fig-scripts/larval_pirna_genes.R
+++ workflow/fig-scripts/larval_pirna_genes.R
@@ -13,7 +13,39 @@ rename.table <- read_tsv('results/figs/celltype_rename_table.tsv') %>%
 w1118.obs <- open_dataset("results/finalized/larval-w1118-testes/obs", format='arrow')
 w1118.expr <- open_dataset("results/finalized/larval-w1118-testes/expr", format='arrow')
 
-markers <- c('piwi','aub','AGO3')
+#markers <- c('piwi','aub','AGO3',"vas","mael","rhi","del","cuff","tud","qin","spn-E","krimp","tej","vret","papi","fs(1)Yb","BoYB","SoYb","egg")
+
+markers <- c("piwi",
+             "qin",
+             "vas",
+             "AGO3",
+             "armi",
+             "aub",
+             "csul",
+             "Gasz",
+             "Hen1",
+             "krimp",
+             "mino",
+             "Nbr",
+             "shu",
+             "spn-E",
+             "vls",
+             "vret",
+             "zuc",
+             "cuff",
+             "del",
+             "rhi",
+             "moon",
+             "arx",
+             "Panx",
+             "nxf2",
+             "egg",
+             "Su(var)3-3",
+             "wde",
+             "mael",
+             "SoYb",
+             "BoYb",
+             "tej")
 
 df <- map_df(w1118.obs %>% collect() %>% pull(clusters) %>% unique() %>% as.list %>% set_names(.,.),
              ~{filter(w1118.expr, clusters == . & gene_symbol %in% markers) %>% collect()}) %>%
@@ -24,20 +56,24 @@ df <- map_df(w1118.obs %>% collect() %>% pull(clusters) %>% unique() %>% as.list
   #filter(str_detect(clusters2, 'Sperm'))
 
 df <- df %>%
-  left_join(rename.table)  
+  left_join(rename.table) %>%
+  group_by(clusters.rename, gene_symbol) %>%
+  summarize(pct.expressing = sum(expression > 0)/n(), mean.expression=mean(expression))
 
-g <- ggplot(df, aes(clusters.rename,expression, fill=clusters.rename)) +
-  geom_violin(aes(fill=clusters.rename),draw_quantiles = c(0.5),scale = 'width', outlier.shape = NA) +
-  theme_classic() +
-  xlab("") + ylab('log(Normalized UMIs + 1)') +
-  guides(fill=F) +
-  scale_fill_brewer(type='qual', palette = 8) +
-  theme(aspect.ratio = 0.3) +
-  theme(plot.caption= element_text(hjust=0.5, face='italic', size=rel(1.2)),
-        axis.title = element_text(size = rel(1.2)), 
-        axis.text.y = element_text(size=rel(1)),
-        axis.text.x = element_text(size=rel(1.5), angle=90, hjust=1, vjust=0.5)) +
-  facet_wrap(~gene_symbol, ncol = 1, scales='free_y')
+g <- df %>%
+  ggplot(aes(gene_symbol, clusters.rename)) +
+  geom_point(aes(size=pct.expressing, fill=mean.expression), shape=21) +
+  scale_fill_fermenter(palette = 8, direction = 1, name='mean Log1p normalized UMIs', guide=guide_legend(label.position = 'bottom', title.position = 'top')) +
+  scale_size(range=c(0, rel(7)), name='Proportion expressing', guide=guide_legend(label.position = 'bottom', title.position = 'top')) +
+  theme_minimal()  +
+  theme(legend.position = 'bottom') +
+  theme(axis.text.x = element_text(angle=45, hjust=1)) +
+  theme(axis.text.y = element_text(size=rel(1.2),)) +
+  theme(aspect.ratio = 1.5, legend.text = element_text(size=rel(0.5)), legend.title = element_text(size=rel(0.5))) +
+  coord_flip() +
+  xlab('') + ylab('')
+
+g
 
 agg_png(snakemake@output[['png']], width=20, height =10, units = 'in', scaling = 1, bitsize = 16, res = 300, background = 'transparent')
 print(g)
