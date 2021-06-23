277cd464cc1383af9dfe94a340177ca9bf4edf65
mal2017 <matt.a.lawlor@gmail.com>
2021-06-23 19:38:37 -0400

    microscopy images in lfs version control

--- workflow/fig-scripts/figure4.R
+++ workflow/fig-scripts/figure4.R
@@ -13,13 +13,14 @@ tes <- read_rds("results/figs/larval_fish_candidate_umis/larval_fish_candidate_u
   theme(axis.title.y=element_text(size=rel(0.5))) +
   theme(strip.text = element_text(face="italic"))
 
-ph <- image_read("/media/mlawlor/T7/microscopy_figs/210215_accord2_calfluor610_eachm_quasar670_3p4-2.slices_1_1.representative.png") %>% image_ggplot()
+#ph <- image_read("/media/mlawlor/T7/microscopy_figs/210215_accord2_calfluor610_eachm_quasar670_3p4-2.slices_1_1.representative.png") %>% image_ggplot()
+ph <- image_read("resources/210215_accord2_calfluor610_eachm_quasar670_3p4-2.slices_1_1.representative.png") %>% image_ggplot()
 
 ph <- ph + geom_ellipse(aes(x0=1650, y0=1950, a=1550,b=1950, angle=pi*0.9), linetype=2, color="white")
 
-ph <- ph + 
-  annotate("text",300,3950, label="DAPI", color="blue") + 
-  annotate("text",1700,3950, label="EAChm", color="red") + 
+ph <- ph +
+  annotate("text",300,3950, label="DAPI", color="blue") +
+  annotate("text",1700,3950, label="EAChm", color="red") +
   annotate("text",3400,3950, label="ACCORD2", color="green")
 
 layout <-"
@@ -30,7 +31,7 @@ CCCBBBB
 CCCBBBB
 "
 
-p <- tes + y_genes + ph + 
+p <- tes + y_genes + ph +
   plot_annotation(tag_levels = 'A', theme=theme(plot.caption = element_text(hjust=0))) +
   plot_layout(design = layout) &
   theme(plot.tag = element_text(face = 'bold', size=rel(1.5)))
@@ -39,4 +40,3 @@ p <- tes + y_genes + ph +
 ggsave(snakemake@output[[1]], p, width = 10, height = 10)
 
 saveRDS(p,file=snakemake@output[[2]])
-
